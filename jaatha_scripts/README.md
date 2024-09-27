b2467db42bb9b7124e7817ed49b3489440416699
Chyi Yin Gwee <gwee@bio.lmu.de>
2024-09-27 11:51:42 +0200

    linkage info for jaatha

--- jaatha_scripts/README.md
+++ jaatha_scripts/README.md
@@ -5,3 +5,5 @@ Please find further instructions in the README.md in subfolder Rscript.
 The subfolders msprime and src contain python scripts and a c++ function
 that are used by the R scripts in folder [Rscripts](./Rscripts/)
 
+The subfolder [linkage_info](./linkage_info/) contains block site positions and individuals used to estimate linkage. 
+The script to generate the block sites is found [here](../scripts/1.8.1_linkage_jaatha.sh)
--- scripts/1.8.1_linkage_jaatha.sh
+++ scripts/1.8.1_linkage_jaatha.sh
@@ -8,9 +8,7 @@
 #SBATCH --time=2-00:00:00
 #SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
 
-# sbatch 1.8.1_linkage_jaatha.sh 101 2000 134inds_overlapped_filtered_norepeats_hwe_incinvariants_norepeats cor1_cor2to3_cnx1to3_cnx6 30,30,30,10
 # sbatch 1.8.1_linkage_jaatha.sh 101 10000 134inds_overlapped_filtered_norepeats_hwe_incinvariants_norepeats cor1_cor2to3_cnx1to3_cnx6 30,30,30,10
-# sbatch 1.8.1_linkage_jaatha.sh 101 1000 134inds_overlapped_filtered_norepeats_hwe_incinvariants_norepeats cor1_cor2to3_cnx1to3_cnx6 30,30,30,10
 
 conda activate py3.8
 module load vcftools/0.1.14-gcc8
@@ -36,12 +34,12 @@ cd $dat/crow_demography/linkage_info/rawdata
 # grep -w scaffold_${i} genome_HC_allpaths41687_v2.5_annotated.intergenic_sites.norepeats.bed > scaffold_${i}.bed
 # done
 
-cd $dat/crow_demography/linkage_info
+# cd $dat/crow_demography/linkage_info
 #run python script to generate random position and check that it's intergenic throughout in the 2000bp, i.e. x+1999 = TRUE
 #generating 102 blocks and removing scaff 60+78 which are part of chr18 highly differentiated region
-python intergenic_block.py $1 $2
-mv intergenic_scaff_pos.bed intergenic_scaff_pos_${2}.bed
-grep -v "scaffold_60\|scaffold_78" intergenic_scaff_pos_${2}.bed | awk -v OFS="\t" 'BEGIN{print "chrom","chromStart","chromEnd"}'1 > intergenic_scaff_pos_${2}_100blocks.bed
+# python intergenic_block.py $1 $2
+# mv intergenic_scaff_pos.bed intergenic_scaff_pos_${2}.bed
+# grep -v "scaffold_60\|scaffold_78" intergenic_scaff_pos_${2}.bed | awk -v OFS="\t" 'BEGIN{print "chrom","chromStart","chromEnd"}'1 > intergenic_scaff_pos_${2}_100blocks.bed
 
 #generate vcf with only the chosen regions
 vcftools --bed intergenic_scaff_pos_${2}_100blocks.bed --keep 4pop.txt --gzvcf ${dat}/05.1_*/over*/${3}.vcf.gz --recode --out intergenic_block_${2}
@@ -49,7 +47,7 @@ grep -v "##" intergenic_block_${2}.recode.vcf > intergenic_block_${2}.cleaned.vc
 vcftools --min-alleles 2 --vcf intergenic_block_${2}.recode.vcf --recode --out intergenic_block_${2}_variantsitesonly
 rm intergenic_block_${2}.recode.vcf
 
-python3 ${dat}/fastsimcoal2/easySFS/easySFS.py -i intergenic_block_${2}_50ind_variantsitesonly.recode.vcf -p ${4}.pop -a -f --unfolded --proj ${5} -o ${4}_${2}
+python3 ${dat}/fastsimcoal2/easySFS/easySFS.py -i intergenic_block_${2}_variantsitesonly.recode.vcf -p ${4}.pop -a -f --unfolded --proj ${5} -o ${4}_${2}
 mkdir SFS
 cd SFS
 mkdir ${2}bp
