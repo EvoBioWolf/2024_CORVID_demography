9d37c6f781a695f7345085ca2e75851c4b642453
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-02 16:53:34 +0100

    Update
    Updated file paths for reference and data directories.

--- scripts/1.3.1_markdup.sh
+++ scripts/1.3.1_markdup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -l
 #SBATCH -J markdup
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_normal
-#SBATCH --cpus-per-task=8
-#SBATCH --time=2-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
 
 # cd 03_readgrp
 # ls C.corone_*.bam | awk -F "_" '{print $1"_"$2}' | uniq > 00_C.corone.txt
@@ -28,8 +21,8 @@ conda activate biotools
 #picard.jar=2.25.7
 
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2"
+ref="Path/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="Path/04_fresh2"
 
 echo $(date)
 STARTTIME=$(date +%s)
