c30b427b1185aa38e7458713b6c9ba6658aae4af
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-02 17:10:29 +0100

    Refactor file paths in twisstsim script
    Updated file paths to relative references.

--- scripts/1.6.6_twisstsim.sh
+++ scripts/1.6.6_twisstsim.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -l
 #SBATCH -J twisstsim
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_normal
-#SBATCH --cpus-per-task=4
-#SBATCH --time=2-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
-
 # sbatch 1.6.6_twisstsim.sh 3PopModel8x "-g EURns -g SPA -g EURc -g O --outgroup O" 3popsim run2
 # sbatch 1.6.6_twisstsim.sh 3PopModel7x "-g EURns -g SPA -g EURc -g O --outgroup O" 3popsim run2
 
@@ -21,10 +13,10 @@ module load vcftools
 module load bcftools
 conda activate biotools
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2"
-scaff="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/01_probes/scaffold2chr"
-gentools="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/06_results/fst/genomics_general"
+ref="Path/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="Path/04_fresh2"
+scaff="Path/01_probes/scaffold2chr"
+gentools="Path/04_fresh2/06_results/fst/genomics_general"
 
 echo $(date)
 STARTTIME=$(date +%s)
