c928f2637db20f0a9e410048c9c09c1646a3a69a
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-02 17:11:11 +0100

    Update 1.8.1_linkage_jaatha.sh

--- scripts/1.8.1_linkage_jaatha.sh
+++ scripts/1.8.1_linkage_jaatha.sh
@@ -1,24 +1,15 @@
 #!/bin/bash -l
 #SBATCH -J simlinkage
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_normal
-#SBATCH --cpus-per-task=2
-#SBATCH --time=2-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
-
-# sbatch 1.8.1_linkage_jaatha.sh 101 10000 134inds_overlapped_filtered_norepeats_hwe_incinvariants_norepeats cor1_cor2to3_cnx1to3_cnx6 30,30,30,10
 
 conda activate py3.8
 module load vcftools/0.1.14-gcc8
 module load bcftools/1.10.2-gcc8
 module load bedtools2
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2"
-scaff="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/01_probes/scaffold2chr"
-gentools="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/06_results/fst/genomics_general"
+ref="Path/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="Path/04_fresh2"
+scaff="Path/01_probes/scaffold2chr"
+gentools="Path/04_fresh2/06_results/fst/genomics_general"
 
 echo $(date)
 STARTTIME=$(date +%s)
