876a4b7fa3efd46e89d5c623d9db43142bb1fd66
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-02 16:54:34 +0100

    Update
    Updated paths for reference and data files in the script.

--- scripts/1.3.2_mapdamage.sh
+++ scripts/1.3.2_mapdamage.sh
@@ -1,20 +1,13 @@
 #!/bin/bash -l
 #SBATCH -J mapdam
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_normal
-#SBATCH --cpus-per-task=2
-#SBATCH --time=2-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
 
 #cd 04*
 #for i in C.torquatus_X*_cleaned.bam; do base=${i%.bam*}; sbatch ../1.3.2_mapdamage.sh ${base}; done
 
 conda activate mapdamage
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2"
+ref="Path/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="Path/2020__ancientDNA/04_fresh2"
 
 echo $(date)
 STARTTIME=$(date +%s)
