6a07985ce2d0ecccf2a2159d287507bf306a65bb
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-02 17:12:53 +0100

    Update paths in stairway_rescaled script

--- scripts/2.2.2_stairway_rescaled.sh
+++ scripts/2.2.2_stairway_rescaled.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -l
 #SBATCH -J stairwayrescaled
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_production
-#SBATCH --cpus-per-task=2
-#SBATCH --time=12-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
-
 # for i in *.poplist
 # do
 # base=${i%.poplist*}
-# sbatch /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/2.2.2_stairway_rescaled.sh ${base}
+# sbatch Path/04_fresh2/2.2.2_stairway_rescaled.sh ${base}
 # done
 
 conda activate biotools 
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2"
+ref="Path/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="Path/04_fresh2"
 
 echo $(date)
 STARTTIME=$(date +%s)
