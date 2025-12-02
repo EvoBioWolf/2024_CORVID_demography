cd3b3e2f245c41efbb451201fec0478c13c9e05a
Chyiyin Gwee <46178076+kakapocy@users.noreply.github.com>
2025-12-02 16:56:14 +0100

    Update

--- scripts/1.4.1_angsdrecal.sh
+++ scripts/1.4.1_angsdrecal.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -l       
 #SBATCH -J angsdREC
-#SBATCH --get-user-env
-#SBATCH --mail-user=gwee@biologie.uni-muenchen.de
-#SBATCH --clusters=biohpc_gen
-#SBATCH --partition=biohpc_gen_production
-#SBATCH --cpus-per-task=30
-#SBATCH --time=8-00:00:00
-#SBATCH -o /dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/slurms/slurm-%j-%x.out
 
 # sbatch 1.4.1_angsdrecal.sh 134inds angsdrecalq30mindepth400genodepth3 poplist_134_angsd 121 400
 #final selected filter: angsdrecalq30mindepth400genodepth3 134inds
@@ -16,8 +9,8 @@ module load vcftools/0.1.14-gcc8
 module load bcftools/1.10.2-gcc8
 conda activate biotools
 
-ref="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
-dat="/dss/dsslegfs01/pr53da/pr53da-dss-0018/projects/2020__ancientDNA/04_fresh2"
+ref="Path/04_fresh2/genome_HC_allpaths41687_v2.5_chrW.fasta"
+dat="Path/04_fresh2"
 
 echo $(date)
 STARTTIME=$(date +%s)
